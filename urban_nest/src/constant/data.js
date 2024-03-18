6d31f103c4e808f43205d55a9c3bfdd93886c51c
Karan <karanpashte153@gmail.com>
2024-03-18 12:25:38 +0530

    Product section copleted

---
+++ urban_nest/src/constant/data.js
@@ -0,0 +1,24 @@
+const shortdata=[
+    {
+        "name":"cotsaruca black sofa",
+        "price":"$300",
+        "url":require('../images/sofa.jpg')
+    },
+    {
+        "name":"magnoot aemless white chair",
+        "price":"$20",
+        "url":require('../images/chair.jpg')
+    },
+    {
+        "name":"four leged table",
+        "price":"$20",
+        "url":require('../images/table.jpg')
+    },
+    {
+        "name":"vatanota table lamp",
+        "price":"$20",
+        "url":require('../images/lamp.jpg')
+    }
+]
+
+export default shortdata;
