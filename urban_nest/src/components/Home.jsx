9a94308a21a22d43209eb99787ac22b7360c7007
Karan <karanpashte153@gmail.com>
2024-03-18 11:03:52 +0530

    Home page done

---
+++ urban_nest/src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from './Navbar'
+import { Box, Typography } from '@mui/material'
+import homebg from '../images/homebg.jpg'
+export default function Home() {
+  return (
+    <Box sx={{backgroundImage:`url(${homebg})`,height:"100vh",width:"100vw",backgroundSize:"cover"}} >
+      <Navbar/>
+      <Box display="flex" justifyContent="center"  marginTop="6rem" >
+        <Box width="45vw" color="white" display="flex" flexDirection="column" justifyContent="center">
+            <Box>
+                <Typography color="#FFFDD0" variant='h2'  >Design Your Sanctuar</Typography> 
+            </Box>
+            <Typography variant='h4'>Unleash Your Style with Us</Typography>
+            <Typography paddingTop="1rem">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit unde omnis eaque quos saepe ipsa et quas totam, a vero ad accusamus architecto natus ab consectetur iusto magnam, nisi ea?</Typography>
+            <Box margin="1rem 0" bgcolor="white" color="black" padding="0.5rem 1rem" fontWeight="bold" borderRadius="0.5rem" textAlign="center" width="10vw">
+                Shop Categories
+            </Box>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
